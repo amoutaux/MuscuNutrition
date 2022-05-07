764cd50bf29f33db6347170887038873aa078f3e
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2022-05-07 23:34:22 +0200

    Implement MealTable & ObjectiveTable component

---
+++ src/components/MealTable.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {DataTable} from 'react-native-paper';
+
+const COLUMNS = {
+  aliment: 'ALIMENT',
+  quantity: 'QTT',
+  prot: 'PROT',
+  lip: 'LIP',
+  glu: 'GLUC',
+  kcal: 'KCAL',
+};
+
+const MealTable = props => {
+  return (
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>{COLUMNS.aliment}</DataTable.Title>
+        <DataTable.Title>{COLUMNS.quantity}</DataTable.Title>
+        <DataTable.Title>{COLUMNS.prot}</DataTable.Title>
+        <DataTable.Title>{COLUMNS.lip}</DataTable.Title>
+        <DataTable.Title>{COLUMNS.glu}</DataTable.Title>
+        <DataTable.Title>{COLUMNS.kcal}</DataTable.Title>
+      </DataTable.Header>
+      {props.entries
+        ? props.entries.map((entry, index) => {
+            return (
+              <DataTable.Row key={index}>
+                <DataTable.Cell>{entry.aliment}</DataTable.Cell>
+                <DataTable.Cell>{entry.quantity}</DataTable.Cell>
+                <DataTable.Cell>{entry.prot}</DataTable.Cell>
+                <DataTable.Cell>{entry.lip}</DataTable.Cell>
+                <DataTable.Cell>{entry.glu}</DataTable.Cell>
+                <DataTable.Cell>{entry.kcal}</DataTable.Cell>
+              </DataTable.Row>
+            );
+          })
+        : null}
+    </DataTable>
+  );
+};
+
+export default MealTable;
---
+++ src/components/ObjectiveTable.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {DataTable} from 'react-native-paper';
+
+const COLUMNS = {
+  type: 'TYPE',
+  gram: 'G',
+  kcal: 'KCAL',
+  totalKcalPercentage: '% kcal total',
+};
+
+const ObjectiveTable = props => {
+  return (
+    <View>
+      <Text>{(props.objective || '') + ' ' + props.totalKcal + ' kcal'}</Text>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>{COLUMNS.type}</DataTable.Title>
+          <DataTable.Title>{COLUMNS.gram}</DataTable.Title>
+          <DataTable.Title>{COLUMNS.kcal}</DataTable.Title>
+          <DataTable.Title>{COLUMNS.totalKcalPercentage}</DataTable.Title>
+        </DataTable.Header>
+        {props.entries
+          ? props.entries.map((entry, index) => {
+              return (
+                <DataTable.Row key={index}>
+                  <DataTable.Cell>{entry.type}</DataTable.Cell>
+                  <DataTable.Cell>{entry.gram}</DataTable.Cell>
+                  <DataTable.Cell>{entry.kcal}</DataTable.Cell>
+                  <DataTable.Cell>{entry.totalKcalPercentage + '%'}</DataTable.Cell>
+                </DataTable.Row>
+              );
+            })
+          : null}
+      </DataTable>
+    </View>
+  );
+};
+
+export default ObjectiveTable;
