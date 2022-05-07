949d521263278f1237eb0e4bdf996a3cd3f91537
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2022-05-08 01:16:08 +0200

    Update MealsScreen with ObjectiveTables

--- src/navigation/MealsScreen.js
+++ src/navigation/MealsScreen.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View} from 'react-native';
+
+import {UserContext} from '../UserContextManagement.js';
+import ObjectiveTable from '../components/ObjectiveTable.js';
+import {getObjectives} from '../utils.js';
+import styles from '../styles/MealsScreen.style.js';
 
 const MealsScreen = () => {
+  const user = useContext(UserContext);
+  const weightGainObjectives = getObjectives(
+    user.protMultiplier,
+    user.lipMultiplier,
+    user.fatPercentage,
+    user.weightKg,
+    user.weightGainObjective,
+  );
+  const weightLossObjectives = getObjectives(
+    user.protMultiplier,
+    user.lipMultiplier,
+    user.fatPercentage,
+    user.weightKg,
+    user.weightLossObjective,
+  );
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Meals Screen</Text>
+    <View style={styles.container}>
+      <ObjectiveTable
+        objective={'seche'}
+        totalKcal={user.weightLossObjective}
+        entries={weightLossObjectives}
+      />
+      <ObjectiveTable
+        objective={'prise de masse'}
+        totalKcal={user.weightGainObjective}
+        entries={weightGainObjectives}
+      />
     </View>
   );
 };
